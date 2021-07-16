e01e58d609ba5e9ea4fe7072ef668bb5f6f126a8
Vinod <vinodh.ram41@gmail.com>
2021-07-16 12:57:29 +0530

    Push

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,19 @@
 // Add your javascript here
 // Don't forget to add it into respective layouts where this js file is needed
 
-$(document).ready(function() {
-  AOS.init( {
+$(document).ready(function () {
+  AOS.init({
     // uncomment below for on-scroll animations to played only once
     // once: true  
   }); // initialize animate on scroll library
 });
 
 // Smooth scroll for links with hashes
-$('a.smooth-scroll')
-.click(function(event) {
+$('a.smooth-scroll').click(function (event) {
   // On-page links
   if (
-    location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-    && 
+    location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+    &&
     location.hostname == this.hostname
   ) {
     // Figure out element to scroll to
@@ -26,7 +25,7 @@ $('a.smooth-scroll')
       event.preventDefault();
       $('html, body').animate({
         scrollTop: target.offset().top
-      }, 1000, function() {
+      }, 1000, function () {
         // Callback after animation
         // Must change focus!
         var $target = $(target);
@@ -34,7 +33,7 @@ $('a.smooth-scroll')
         if ($target.is(":focus")) { // Checking if the target was focused
           return false;
         } else {
-          $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+          $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
           $target.focus(); // Set focus again
         };
       });
