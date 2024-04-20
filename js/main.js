9ec49058d7f89298622784a05372e10584b10759
Vinodh Ram <vinod-nodejs@webappclouds.com>
2024-04-20 18:00:33 +0530

    Link details

--- js/main.js
+++ js/main.js
@@ -47,3 +47,14 @@ function closePopup(elementId) {
     var modal = document.getElementById(elementId);
     modal.style.display = "none";
 }
+
+function openLocation() {
+    var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+    if (/android/i.test(userAgent)) {
+        window.location.href = "geo:0,0?q=Hyderabad, Telangana, India";
+    } else if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+        window.location.href = "maps://maps.google.com/maps?q=Hyderabad, Telangana, India";
+    } else {
+        window.open("https://www.google.com/maps?q=Hyderabad, Telangana, India", "_blank");
+    }
+}
